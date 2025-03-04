316f1d7de4958e00e8ecdb2c5604bec5cf6e664b
NeneNafe <nenechiwaya@gmail.com>
2025-03-04 22:01:25 +0200

    A loop that return hash

---
+++ javascript/README.md
@@ -0,0 +1,3 @@
+# JavaScript Practice Exercises
+
+This folder contains all the exercises i took while re-leaning JavaScript. So welcome!!!
---
+++ javascript/exercise1.js
@@ -0,0 +1,8 @@
+/* A code that takes # and prints it through 7 calls */
+
+let num = 7;
+let string = "";
+for (i = 0; i < num; i++) {
+    string += "#";
+    console.log(string);
+}
