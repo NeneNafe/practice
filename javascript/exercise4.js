7fafd7798081c2f70c18e0a42fb1659a69750574
NeneNafe <nenechiwaya@gmail.com>
2025-05-07 09:30:38 +0200

    A function that return the minimum between two numbers

---
+++ javascript/exercise4.js
@@ -0,0 +1,11 @@
+/*
+- function that take 2 arguments and returns their minimum 
+*/
+
+function min(base, exponent) {
+    return (Math.min(base, exponent)+ 90);
+}
+console.log("The minimum is",min(2, 3));
+console.log("The minimum is",min(13, 19));
+console.log("The minimum is",min(15.9, 14.5));
+console.log("Good Job! you found all the minimum values!");
